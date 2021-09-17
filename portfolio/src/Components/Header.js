22f1250297a3aab62b5c45faed7b34aca87885a0
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-09-17 18:29:07 -0300

    Header

--- portfolio/src/Components/Header.js
+++ portfolio/src/Components/Header.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import Logo from '../images/Logo.jpg';
+import '../Styles/Header.css';
 
 function Header() {
   return (
-    <h1>Header</h1>
+    <div className="header-main">
+      <div>
+        <h1>Hey</h1>
+        <h2>I'm Bruno, a software developer from Brazil</h2>
+        <button className="btn-main" type="button">Get in Touch</button>
+      </div>
+      <div>
+        <img src={ Logo } alt="Logo" width="390px" />
+      </div>
+    </div>
   )
 }
 
